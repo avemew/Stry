f7216d4d66a5610170017685628b11c2462d5a3b
Abud <ab_du@uni-bremen.de>
2022-12-15 14:39:54 +0100

    rain+onload done

--- stry/src/components/stry.vue
+++ stry/src/components/stry.vue
@@ -1,9 +1,4 @@
 <script>
-
-function myFunction() {
-  alert("Page is loaded");
-}
-
 import moment from "moment";
 
 //canvas settings
@@ -19,16 +14,19 @@ export default {
     };
   },
 
-
+  created() {
+    this.getWeatherData()
+    this.setBackground()
+    this.getRainData()
+  },
 
   mounted: function () {
     window.setInterval(() => {
       //if(0>0){
-      $('body').ripples("drop", getRandomX(), getRandomY(), calculateSize(1), 1);
-    //}
+      $('body').ripples("drop", getRandomX(), getRandomY(), calculateSize(this.rainAverageValue()+5), 1);
+      //}
     }, calculateTimeout(1))
   },
-
 
   methods: {
     //Returns API Url for the current day
@@ -37,33 +35,6 @@ export default {
       //builds URL-String with BaseURL and the truncatedDate
       return 'https://api.open-meteo.com/v1/forecast?latitude=53.08&longitude=8.81&hourly=apparent_temperature&start_date=' + truncatedDate + '&end_date=' + truncatedDate;
     },
-    getTodayUrlRain(){
-      let truncatedDateRain = this.todayDate().slice(0,10); //cuts off the timestamp from todayDate
-      //builds URL-String with BaseURL and the truncatedDate
-      return 'https://api.open-meteo.com/v1/forecast?latitude=53.08&longitude=8.81&hourly=rain&start_date=' + truncatedDateRain + '&end_date=' + truncatedDateRain;
-    },
-
-    getRainData() {
-      //fetches WeatherData from today's URL and stores it in weatherDataList
-      fetch(this.getTodayUrlRain())
-          .then(response => (response.json()))
-          .then(data => (data["hourly"]))
-          .then(data => this.getRainMap(data))
-          .then(data => (this.rainDataList = data))
-    },
-
-
-
-    getRainMap(timeWeatherLists) {
-      const timesArray = Array.from(timeWeatherLists["time"])
-      const tempArray = Array.from(timeWeatherLists["rain"])
-
-      return timesArray.reduce((previousValue, currentValue, currentIndex) => {
-        return Object.assign(previousValue, {[currentValue]: tempArray.at(currentIndex)})
-
-      }, {})
-    },
-
     getWeatherData() {
       //fetches WeatherData from today's URL and stores it in weatherDataList
       fetch(this.getTodaysUrl())
@@ -80,15 +51,9 @@ export default {
         return Object.assign(previousValue, {[currentValue]: tempArray.at(currentIndex)})
       }, {})
     },
-
     showWeatherByDate(date) {
       //used to display the weatherDataList on the Website
       return this.weatherDataList[date]
-    },
-
-    showRainByDate(date) {
-      //used to display the weatherDataList on the Website
-      return this.rainDataList[date]
     },
 
     //returns the current date+hour
@@ -98,7 +63,7 @@ export default {
     },
 
     //calculates average Value of the weatherDataList
-    averageValue() {
+    tempAverageValue() {
       let sum = 0;
       let count = 0;
 
@@ -119,11 +84,53 @@ export default {
 
     setBackground() {
       //hue - color value; Color from Average Temp gets calculated via polynomialInterpolationRemap
-      let hue = polynomialInterpolationRemap(this.averageValue());
+      let hue = polynomialInterpolationRemap(this.tempAverageValue());
       //color the background square with the given hue
       context.fillStyle = 'hsl(' + [hue, '100%', '50%'] + ')';
       context.fillRect(0, 0, canvas.width, canvas.height);
     },
+    //Rain Data
+    getTodayUrlRain() {
+      let truncatedDateRain = this.todayDate().slice(0, 10); //cuts off the timestamp from todayDate
+      //builds URL-String with BaseURL and the truncatedDate
+      return 'https://api.open-meteo.com/v1/forecast?latitude=53.08&longitude=8.81&hourly=rain&start_date=' + truncatedDateRain + '&end_date=' + truncatedDateRain;
+    },
+    getRainmap(rainlists) {
+      const timesArray = Array.from(rainlists["time"])
+      const rainArray = Array.from(rainlists["rain"])
+      // console.log(rainArray)
+
+      return timesArray.reduce((previousValue, currentValue, currentIndex) => {
+        return Object.assign(previousValue, {[currentValue]: rainArray.at(currentIndex)})
+      }, {})
+    },
+    getRainData() {
+      //fetches WeatherData from today's URL and stores it in weatherDataList
+      fetch(this.getTodayUrlRain())
+          .then(response => (response.json()))
+          .then(data => (data["hourly"]))
+          .then(data => this.getRainmap(data))
+          .then(data => (this.rainDataList = data))
+    },
+    rainAverageValue() {
+      let sum = 0;
+      let count = 0;
+
+      //sum up all values, increment count every item
+      for (const item in this.rainDataList) {
+        sum += this.rainDataList[item];
+        ++count;
+      }
+      sum /= count; //average
+
+      //protection for empty list case
+      if (isNaN(sum)) {
+        return 0;
+      }
+      return sum;
+    },
+
+
 
   },
 
@@ -156,54 +163,42 @@ function polynomialInterpolationRemap(value) {
   )
 }
 
-function getRandomX(){
+function getRandomX() {
   return Math.floor(Math.random() * screen.width);
 }
 
-function getRandomY(){
+function getRandomY() {
   return Math.floor(Math.random() * screen.height);
 }
 
-function calculateSize(rainInMm){
-  return Math.round(2.49 * rainInMm + 0.1)*2; //see https://www.wolframalpha.com/input?i2d=true&i=interpolating+polynomial+%7B0%2C0.1%7D%5C%2844%29%7B10%2C25%7D
+function calculateSize(rainInMm) {
+  return Math.round(2.49 * rainInMm + 0.1) * 2; //see https://www.wolframalpha.com/input?i2d=true&i=interpolating+polynomial+%7B0%2C0.1%7D%5C%2844%29%7B10%2C25%7D
 }
+
 //mapping for timeout
-function calculateTimeout(rainInMm){
-  return Math.round((-6.68687 * Math.pow(rainInMm, 3) + 19.0424 * Math.pow(rainInMm, 2) + 488.163 * rainInMm + 1)/3); //see https://www.wolframalpha.com/input?i2d=true&i=interpolating+polynomial+%7B0%2C1%7D%5C%2844%29%7B0.1%2C50%7D%5C%2844%29%7B2%2C1000%7D%5C%2844%29%7B10%2C100%7D
+function calculateTimeout(rainInMm) {
+  return Math.round((-6.68687 * Math.pow(rainInMm, 3) + 19.0424 * Math.pow(rainInMm, 2) + 488.163 * rainInMm + 1) / 3); //see https://www.wolframalpha.com/input?i2d=true&i=interpolating+polynomial+%7B0%2C1%7D%5C%2844%29%7B0.1%2C50%7D%5C%2844%29%7B2%2C1000%7D%5C%2844%29%7B10%2C100%7D
 }
 
 </script>
 <template>
-  <body onload="myFunction()">
-  <div id="weatherData">
+  <header>
+    <h1>Average Value: {{ tempAverageValue() }}</h1>
+<!--    <h1>Average Value: {{ rainAverageValue() }}</h1>-->
 
-    <h1>My Weather App</h1>
+  </header>
 
-    <button v-on:click="getWeatherData">Get Weather Data</button>
-    <button v-on:click="setBackground">set Background</button>
-    <br>
-    Average Value: {{ averageValue() }}
-    <br>
-    {{ showWeatherByDate(todayDate()) }}
-    {{ showRainByDate(todayDate())}}
-    
-
-    <li v-for=" (item , index) in weatherDataList">
-      {{ index }} - {{ item }}
-    </li>
-
-  </div>
-  </body>
 </template>
 
 <style scoped>
 h1 {
+  padding: 400px;
   color: white;
 }
 
-#weatherData {
+weatherData {
   color: darkturquoise;
-  background-color: transparent;
+  background-color: brown;
   margin: 0;
   padding: 0;
 }
